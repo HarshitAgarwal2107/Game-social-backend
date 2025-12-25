612bba79eeee81c9cc373a4b6a4710c8853cfb94
Harshit Agarwal <harshitagarwal2107@gmail.com>
2025-12-25 07:46:58 +0530

    Fix Linux case-sensitive import issues

--- config/db.js
+++ config/db.js
@@ -7,11 +7,8 @@ let pgPool;
 const connectDB = async () => {
   try {
     // MongoDB
-    const mongoConn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log(`✅ MongoDB connected: ${mongoConn.connection.host}`);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log(`✅ MongoDB connected`);
 
     // PostgreSQL
     pgPool = new Pool({
